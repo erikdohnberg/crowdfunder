8fd1224f76d727fc2867d537fdeeb5baae0ad7d2
Erik Dohnberg <erikdohnberg@gmail.com>
2013-04-09 11:23:48 -0400

    Project#index built using integration test and TDD

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,11 @@ class ActiveSupport::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
 
-  # Add more helper methods to be used by all tests here...
+  # Add more helper methods to be used by all tests here..
+
+  class ActionDispatch::IntegrationTest
+  	include Capybara::DSL
+  	Capybara.app = Crowdfunder::Application
+  end
+
 end
