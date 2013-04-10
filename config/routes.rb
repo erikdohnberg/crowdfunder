79491a65465926b8600ecaef35d8e307d79ae18d
Erik Dohnberg <erikdohnberg@gmail.com>
2013-04-10 10:42:16 -0400

    Fixed routes. Added to corcery and application HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Crowdfunder::Application.routes.draw do
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   resources :projects
-  resources :users, :except => [:index, :destroy]
-  root :to => "home#index"
-  
+
+  resources :users, except: [:index]
+
+  # Sign in and sign out actions
+  resource :session, :only => [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
