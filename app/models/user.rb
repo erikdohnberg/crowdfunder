2cf0e171dfe254119c827ee8fbf7591ec4687090
Erik Dohnberg <erikdohnberg@gmail.com>
2013-04-10 09:16:11 -0400

    Add test for registration and add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  authenticates_with_sorcery!
-  has_many :projects
-  # attr_accessible :title, :body
+	authenticates_with_sorcery!
+	has_many :projects
+
+	validates :first_name, :presence => true
+	validates :last_name, :presence => true
+	validates :email, :presence => true
+	validates :password, :presence => true, :on => :create
+	
+	# attr_accessible :title, :body
 end
