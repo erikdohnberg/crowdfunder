0efc134836df0c8ce8f0b3144035ee9f9f77d207
Erik Dohnberg <erikdohnberg@gmail.com>
2013-04-11 17:33:06 -0400

    Writing mailer for new pledges to project owner (in process)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,15 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@crowdfunder.com"
-  
+
+  def new_pledge(pledge)
+  	@owner = pledge.project.user
+  	@project = pledge.project
+  	@pledge = pledge
+  	@backer = pledge.user
+
+  	mail(:to => owner.email, :subject => "New pledge for your project!")
+  end
+
+  # def forgot_password(user)
+  # end
 end
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -5,7 +5,14 @@ class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
+  after_create :send_new_pledge_email
+
   validates :amount,  :numericality => {:only_integer => true, :greater_than => 0}, :presence => true
   validates :user, :presence => true
   validates :project, :presence => true
+
+  def send_new_pledge_email
+  	UserMailer.new_pledge(self).deliver
+  end
+
 end
